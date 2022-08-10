8888f84d4745c3f319060fe1c4b9cced4a302c51
Danelle <rhowia@gmail.com>
2022-08-10 18:59:27 -0400

    Completed exercises 1 through 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // take the string and make it into an array
+    let arrayString = string.split("");
+    let revStr = "";
+    // reverse the array
+    // then attach a string version of the last letters
+    for (let i = arrayString.length - 1; i >= 0; i--) {
+        revStr += arrayString[i].toString();
+    }
+    return revStr;
 };
 
 // Do not edit below this line
