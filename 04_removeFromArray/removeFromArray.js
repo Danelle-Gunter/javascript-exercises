c6ece8591d4027715ef7ae00ddefa9348d5fe426
Danelle <rhowia@gmail.com>
2022-08-11 16:43:07 -0400

    Completed exercises 4 through 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    // first element will be the array
+    const array = args[0];
+    // newArray will store the array with the items taken away
+    const newArray = [];
+    
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
