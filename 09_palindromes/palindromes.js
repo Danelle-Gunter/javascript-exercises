578f801465c6b27b8bd3907dc051b0346a58ddad
Danelle <rhowia@gmail.com>
2022-08-24 12:51:49 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    
+    for (let i = 0; i < cleanStr.length/2 ; i++) {
+        if (cleanStr[i] !== cleanStr[cleanStr.length - 1 - i]) {
+            return false;
+        }
+    }
 
+    return true;
+    // const reverseStr = cleanStr.split('').reverse().join('');
+    // return reverseStr == cleanStr;
 };
 
 // Do not edit below this line
