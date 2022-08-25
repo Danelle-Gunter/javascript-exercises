32516670a08ca23f1a0e90d7df4e67c2a6e5bf0e
Danelle <rhowia@gmail.com>
2022-08-25 15:09:27 -0400

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+  // the number is the sum of the two proceeding numbers
+  // ie: 1, 1, 2, 3, 5, 8, 13
+  // returns the nth number of the series
+  
+  if (nth < 0) return "OOPS";
+  if (nth === 0) return 0;
+  
+  let first = 0;
+  let second = 1;
 
+  for (let i = 1; i < nth; i++) {
+    const temp = second;
+    second = first + second;
+    first = temp;
+  }
+
+  return second;
+  
 };
 
 // Do not edit below this line
